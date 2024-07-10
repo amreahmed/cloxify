fc8996e06df99ab6f07db060e1c6aa39cdfc88da
Amr Ezzat <amr.2023001640@buc.edu.eg>
2024-07-10 08:35:41 +0300

    chore: Update AddTransformationsTypePage and Header components
    - Update AddTransformationsTypePage component to import Header from "@/components/shared/Header"
    - Update Header component to include title and subtitle props
    - Refactor code to improve readability and maintainability

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import React from "react";
+import {  transformationTypes } from "@/constants";
 
-const AddTransformationsTypePage = () => {
-  return (
-    <div>AddTransformationsTypePage</div>
-  )
-}
+const AddTransformationsTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return <Header title={transformation.title} subtitle={transformation.subTitle} />;
+};
 
-export default AddTransformationsTypePage
+export default AddTransformationsTypePage;
